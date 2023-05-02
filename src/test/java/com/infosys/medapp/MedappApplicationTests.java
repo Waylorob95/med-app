d343aeac17415041120acf0c4aa65694a6168a66
Stanimir Todorov <Stanimir.Todorov@ad.infosys.com>
2023-05-02 21:55:46 +0300

    First commit

---
+++ src/test/java/com/infosys/medapp/MedappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.medapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
